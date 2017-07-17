87fe8fab1a6586c8332d912a2469415070b6231f
kexiaojiu <kexiaojiu@163.com>
2017-07-17 23:44:20 +0800

    add base.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 #conding=utf-8
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """显示所有的主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request, 'learning_logs/topics.html', context)
