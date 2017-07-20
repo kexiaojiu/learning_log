ac520f9abf15acdcde320ffaa5435e2fb2130c9b
kexiaojiu <kexiaojiu@163.com>
2017-07-20 23:42:15 +0800

    make users owning their data

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Topic(models.Model):
     """用户学习的主题"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True) 
+    owner = models.ForeignKey(User)
     
     def __str__(self):
         """返回模型的字符串表示"""
