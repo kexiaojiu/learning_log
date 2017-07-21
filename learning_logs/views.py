05ec379e19f1c9ddadf11810b7aa0f42fa3a1275
kexiaojiu <kexiaojiu@163.com>
2017-07-21 19:34:42 +0800

    protect users' webpages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #conding=utf-8
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import Topic, Entry
@@ -24,6 +25,10 @@ def topics(request):
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求的主题属于当前用户
+    #~ if topic.owner != request.user:
+    if  check_topic_owner(topic, request) == False:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -39,7 +44,9 @@ def new_topic(request):
         # POST提交的数据，对数据进行处理
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
             
     context = {'form': form}
@@ -73,6 +80,10 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
     
+    #~ if topic.owner != request.user:
+    if  check_topic_owner(topic, request) == False:
+        raise Http404
+        
     if request.method != 'POST':
         # 初次请求，使用当前条目填充表单
         form = EntryForm(instance=entry)
@@ -85,3 +96,10 @@ def edit_entry(request, entry_id):
                                         args=[topic.id]))
     context = {'entry':entry, 'topic':topic, 'form':form}
     return render(request, 'learning_logs/edit_entry.html', context)    
+
+def check_topic_owner(topic, request):
+    """核实主题关联到的用户为当前用户"""
+    if topic.owner == request.user:
+        return True
+    else:
+        return False
